d86ce6926a86d2d6ea671675e4bae374db94ac8a
EliN4til <joelito1gs@gmail.com>
2025-10-26 18:31:23 +0000

    Update conectar.py

--- conectar.py
+++ conectar.py
@@ -2,19 +2,19 @@ import sqlite3
 
 def conectar():
     """
-    Establece conexión con la base de datos SQLite y activa claves foráneas.
+    Establece conexión con la base de datos de SQLite "Empresa.db" y activa las claves foraneas, (me estaba dando error, no se porque, a la hora de crear una nueva db).
     
     Devuelve:
-        tupla: (conexión, cursor) o (None, None) en caso de que haya un error
+        una tupla: (conexión, cursor) o (None, None) en caso de que haya un error de conexión a la base de datos.
     """
     try:
         con = sqlite3.connect("Empresa.db")
         con.execute("PRAGMA foreign_keys = ON;")
         cur = con.cursor()
-        print("✅ Conexión establecida correctamente")
+        print("Conexión establecida correctamente")
         return con, cur
     except sqlite3.Error as e:
-        print(f"❌ Error al conectar a la base de datos: {e}")
+        print(f"Error al conectar a la base de datos: {e}")
         return None, None
 
 if __name__ == "__main__":
